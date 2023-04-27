8f31c942b504d6d654a199078d45513de85dc9da
Ludovic <ludovic@localhost.localdomain>
2023-04-27 18:12:16 +0200

    mise à jour script git

--- switch_to_branch.sh
+++ switch_to_branch.sh
@@ -2,111 +2,109 @@
 # Passe toutes les branche git à la valeur passée
 branch="$1"
 
-# Mets à jour tous les repository Mercurial
-echo "Commiting Mercurials..."
-cd /srv/projets/gmcd/courrielleur_sources/cmel_tb60.9.1
-hg commit -m "Commited by script"
-echo "----------------------------"
-
 echo "Switching branch for Sources"
 cd /srv/projets/gmcd/courrielleur_sources/cmel_tb60.9.1
 git checkout $branch
 git pull
 git branch
+echo "Commiting Mercurials..."
+hg commit -m "Commited by script"
 echo "----------------------------"
 
 echo "Switching branch for Anais"
 cd /srv/projets/anaismoz/courrielleur_anais
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for Archibald"
 cd /srv/projets/archibald/courrielleur_archibald
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for Courrielleur"
 cd /srv/projets/courrielleur/courrielleur_courrielleur
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for Contacts"
 cd /srv/projets/ContactsDAV/courrielleur_contacts
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for Lightning"
 cd /srv/projets/Lightning/courrielleur_lightning
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for Pacome"
 cd /srv/projets/pacome/courrielleur_pacome
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for Commentaires"
 cd /srv/projets/commentaires/courrielleur_commentaires
 git checkout $branch
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for Melanissimo"
 cd /srv/projets/cm2m2ssimo/courrielleur_melanissimo
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for Webapps"
 cd /srv/projets/WebAppsM2/courrielleur_webapps
 git checkout $branch
-git pull
+git pull -all
 git branch
+echo "----------------------------"
 
 echo "Switching branch for Signature"
 cd /srv/projets/signature/courrielleur_signature
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for Mailredirect"
 cd /srv/projets/mailredirect/courrielleur_mailredirect
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for ManuallySortFolders"
 cd /srv/projets/manually_sort_folders/courrielleur_manually_sort_folders
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for Lookout"
 cd /srv/projets/lookout/courrielleur_lookout
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
 
 echo "Switching branch for ColorFolders"
 cd /srv/projets/color_folders/courrielleur_color_folders
 git checkout $branch
-git pull
+git pull -all
 git branch
 echo "----------------------------"
